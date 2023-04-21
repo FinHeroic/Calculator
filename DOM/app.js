524a8a66f77391fa5131559cb574bfb32e08e674
Finley Joseph <finleyjoseph28@gmail.com>
2023-04-21 13:56:50 -0400

    f

--- DOM/app.js
+++ DOM/app.js
@@ -1,20 +1,20 @@
 (function(){
 
-  let screen = document.querySelector('screen')
-  let buttons = document.querySelector('.btn')
+  let screen = document.querySelector('.screen')
+  let buttons = document.querySelectorAll('.btn')
   let clear = document.querySelector('.btn-clear')
   let equal = document.querySelector('.btn-equal')
 
-  buttons.forEach(function (button) {
-    button.addEventListener('click', function (e) {
+  buttons.forEach(function(button) {
+    button.addEventListener('click', function(e) {
       let value = e.target.dataset.num;
       screen.value += value;
     })
   });
 
-  equal.addEventListener('click', function (e) {
-    if (screen.value === '') {
-      screen.value = "";
+  equal.addEventListener('click', function(e) {
+    if(screen.value === '') {
+      screen.value = "Please enter";
     } else {
       let answer = eval(screen.value);
       screen.value = answer;
